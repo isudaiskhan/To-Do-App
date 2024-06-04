516553eb8f5c61450bdad21f6695b0df86b56e52
sudais khan <isudaiskhan54@gmail.com>
2024-06-04 18:15:42 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import TodoList from "./Components/TodoList/TodoList";
+
+function App() {
+  return (
+    <>
+
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+
+    </>
+  );
+}
+
+export default App;
